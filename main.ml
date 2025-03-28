c2635eb1db46775e9dcc86c1f50c7474e8ff340c
ajuncosa <anajuncosa97@gmail.com>
2025-03-28 22:33:11 +0100

    Feat: cli

--- main.ml
+++ main.ml
@@ -1,17 +1,18 @@
-let jsonfile_param =
-  let open Core.Command.Param in
-  anon ("jsonfile" %: string)
+let run_machine description input =
+  let machine = Parser.parse_machine_description description in
+  print_endline (Machine.to_string machine)
 
 let command =
   Core.Command.basic
     ~summary:"Wow this is a very cool Turing machine"
-    ~readme:(fun () -> "More detailed information")
-    (Core.Command.Param.map jsonfile_param ~f:(fun filename () ->
-          print_endline ("FILE:" ^ filename)))
+    Core.Command.Param.(map 
+      (both
+        (anon ("jsonfile" %: string))
+        (anon ("input" %: string)))
+      ~f:(fun (jsonfile, input) () ->
+        run_machine jsonfile input))
 
 let () =
   Command_unix.run command;
-  let machine = Parser.parse_machine_description "machine_descriptions/unary_sub.json" in
-  print_endline (Machine.to_string machine)
     (* Format.printf "Parsed to %a" Json.pp (parse_json "machine_descriptions/unary_sub.json") *)
 
