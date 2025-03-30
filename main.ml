3f473c4fa39a324d0c6152dc0426cf4ab4feae48
ajuncosa <anajuncosa97@gmail.com>
2025-03-30 20:23:48 +0200

    Fix: input argument position

--- main.ml
+++ main.ml
@@ -1,19 +1,6 @@
-let run_machine description input =
+let run_turing description input =
   let machine = Parser.parse_machine_description description in
   print_endline (Machine.to_string machine)
-(* let command =
-  Core.Command.basic
-    ~summary:"Wow this is a very cool Turing machine"
-    Core.Command.Param.(map 
-      (both
-        (anon ("jsonfile" %: string))
-        (anon ("input" %: string)))
-      ~f:(fun (jsonfile, input) () ->
-        run_machine jsonfile input))
-
-let () =
-  Command_unix.run command; *)
-    (* Format.printf "Parsed to %a" Json.pp (parse_json "machine_descriptions/unary_sub.json") *)
 
 open Cmdliner
 open Cmdliner.Term.Syntax
@@ -23,14 +10,13 @@ let jsonfile =
 
 let input =
   let doc = "input of the machine" in
-  Arg.(required & pos 0 (some string) None & info [] ~doc ~docv:"INPUT")
-
+  Arg.(required & pos 1 (some string) None & info [] ~doc ~docv:"INPUT")
 
 let ft_turing_cmd =
   let doc = "wow this is a very cool Turing machine" in
-  Cmd.v (Cmd.info "ft_turing" ~doc) @@
+  Cmd.v (Cmd.info "ft_turing" ~doc ~exits:[]) @@
   let+ jsonfile and+ input in
-  run_machine jsonfile input
+  run_turing jsonfile input
 
 let main () = Cmd.eval ft_turing_cmd
 let () = if !Sys.interactive then () else exit (main ())
