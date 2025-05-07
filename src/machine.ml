512bc446185f5080d4aab2316923c91a8c3732dc
Adrian <adrian199909@gmail.com>
2025-05-07 18:46:52 +0200

    Add: Out of tape exception

--- src/machine.ml
+++ src/machine.ml
@@ -18,9 +18,9 @@ let execute_transition (machine : t) (transition : Transition.t) : t =
   } in m
 
 let rec run (machine : t) (description : MachineDescription.t) =
+  if (machine.head_pos < 0) || (machine.head_pos >= (List.length machine.tape)) then failwith "Machine out of tape";
   let transition = Transition.find (List.assoc machine.state description.transitions) (List.nth machine.tape machine.head_pos) in
   print_endline ((to_string machine) ^ " " ^ (Transition.to_string transition));
   let updated_machine = execute_transition machine transition in
   if (List.exists (fun final -> final = updated_machine.state) description.finals) then ()
   else run updated_machine description
-    
